5da4c0a3417733c83141fc48d4564c06d52a9d10
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2026-05-25 21:37:38 -0300

    Obtendo Post por ID

---
+++ src/main/java/com/sampguilherme/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.sampguilherme.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sampguilherme.workshopmongo.domain.Post;
+import com.sampguilherme.workshopmongo.repository.PostRepository;
+import com.sampguilherme.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		if (post.isEmpty()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado.");
+	    }
+		
+		return post.get();
+	}
+	
+}
